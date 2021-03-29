ae81a42fcb175e6e9a1de3edb44bdbf8952792a8
Tina <1098299287@qq.com>
2021-03-29 20:59:35 +0800

    完成路由设置

---
+++ src/views/Labels.vue
@@ -0,0 +1,14 @@
+import Labels from '@/views/Labels.vue';
+<template>
+  <div>
+    Labels.vue
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Labels",
+};
+</script>
+
+<style lang="scss" scoped></style>
