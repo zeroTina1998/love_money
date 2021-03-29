33b9d7bd49641caada6bc4d38ff4acc36643e778
Tina <1098299287@qq.com>
2021-03-29 22:01:23 +0800

    添加404

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from "@/views/Home.vue";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -22,6 +23,10 @@ const routes = [
   {
     path: "/statistics",
     component: Statistics,
+  },
+  {
+    path: "*",
+    component: NotFound,
   },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <hr />
+    <p>当前页面不存在，请检查你的网址是否正确</p>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style lang="scss" scoped></style>
