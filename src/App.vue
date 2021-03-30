b2cbd7a1f478947545b643a532ad63b090ab0384
Tina <1098299287@qq.com>
2021-03-30 18:36:53 +0800

    添加icon样式

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,15 @@
   padding: 0;
   box-sizing: border-box;
 }
+body {
+  line-height: 1.5;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -19,11 +26,9 @@
 }
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
