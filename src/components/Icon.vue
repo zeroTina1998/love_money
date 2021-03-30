caa71b91c8c9f00be84560532e94191785c85375
Tina <1098299287@qq.com>
2021-03-30 17:57:04 +0800

    封装Icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <svg class="icon">
+      <use :xlink:href="'#' + name" />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
+export default {
+  props: ["name"],
+  name: "Icon",
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+  overflow: hidden;
+  vertical-align: -0.15em;
+}
+</style>
