0f22b316ecc5723224e2d7feb0188cc63c03a661
Tina <1098299287@qq.com>
2021-04-05 22:29:01 +0800

    @符号出现错误

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -23,17 +23,18 @@ import { Component, Prop } from "vue-property-decorator";
 export default class Tags extends Vue {
   @Prop() dataSource: string[] | undefined;
   selectedTags: string[] = [];
-  toggle(tag) {
+  toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
       this.selectedTags.splice(index, 1);
     } else {
       this.selectedTags.push(tag);
     }
+    this.$emit("xxx", this.selectedTags);
   }
   create() {
     const name = window.prompt("请输入新的标签名：");
-    if (name === "") {
+    if (name === "" || "&nbsp;") {
       window.alert("标签名不能为空");
     } else if (this.dataSource) {
       this.$emit("update:dataSource", [...this.dataSource, name]);
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,25 +1,29 @@
 <template>
   <Layout class-prefix="layout">
-    <Tags :dataSource.sync="tags" />
+    <Tags :dataSource.sync="tags" @xxx="yyy" />
     <Notes />
     <Types />
     <NumberPad />
   </Layout>
 </template>
 
-<script>
+<script lang="ts  ">
+import Vue from 'vue';
 import NumberPad from "../components/Money/NumberPad.vue";
 import Types from "../components/Money/Types.vue";
 import Notes from "../components/Money/Notes.vue";
-import Tags from "../components/Money/Tags.vue";
-export default {
-  name: "Money",
-  components: { Tags, Notes, Types, NumberPad },
-  data() {
-    return {
-      tags: ["衣", "食", "住", "行"],
-    };
-  },
+import Tags from '../components/Money/Tags.vue';
+import { Component } from 'vue-property-decorator';
+@Component({
+    components: { Tags, Notes, Types, NumberPad }
+    }
+)
+export default class Money extends Vue{
+     tags = ["衣", "食", "住", "行"];
+    yyy(zzz){
+       console.log(zzz);
+       }
+
 };
 </script>
 
