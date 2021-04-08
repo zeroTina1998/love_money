7111e30decb77ed250280b6424105cee21e5ac54
Tina <1098299287@qq.com>
2021-04-08 18:39:34 +0800

    新增Editlabel组件

--- src/views/Editlabel.vue
+++ src/views/Editlabel.vue
@@ -7,9 +7,22 @@
 <script lang="ts">
 import { Component } from "vue-property-decorator";
 import Vue from "vue";
+import tagListModel from "@/models/tagListModel";
 
 @Component
-export default class Editlabel extends Vue {}
+export default class Editlabel extends Vue {
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter((t) => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
