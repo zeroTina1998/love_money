39b6b562f36432a84f219977746e9a473ebc66e5
Tina <1098299287@qq.com>
2021-04-09 14:52:59 +0800

    使用全局tagList出错

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -28,7 +28,7 @@ const tags = tagListModel.fetch();
   components: { Button },
 })
 export default class Labels extends Vue {
-  tags = tagListModel.data;
+  tags = window.tagList;
   created() {}
   createTag() {
     const name = window.prompt("请输入标签名");
