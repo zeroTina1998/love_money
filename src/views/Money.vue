f41dc7448ab383cf7c7e573b0fe6c49a4128c1a5
Tina <1098299287@qq.com>
2021-04-10 17:43:54 +0800

    重新将create封装到model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -50,9 +50,7 @@ export default class Money extends Vue {
     this.record.amount = parseFloat(value);
   }
   saveRecord() {
-    const record2: RecordItem = recordListModel.clone(this.record);
-    record2.createdAt = new Date();
-    this.recordList.push(record2);
+    recordListModel.create(this.record);
   }
   @Watch("recordList")
   onRecordListChanged() {
